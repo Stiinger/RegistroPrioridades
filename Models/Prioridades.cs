6a70e1be9f20544ead22b8a6517c467a0d386154
Stiinger <ortegantony29@gmail.com>
2024-01-14 08:25:34 -0400

    Se agregó el Modelo

---
+++ Models/Prioridades.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPrioridades.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+        [Required(ErrorMessage = "El campo Descripción no puede estar vacío")]
+        public string? Descripcion { get; set; }
+        [Required(ErrorMessage = "El campo Días Compromiso no puede estar vacío")]
+        [Range(1, 365, ErrorMessage = "Debe ingresar valores entre 1 y 365")]
+        public int? DiasCompromiso { get; set; }
+    }
+}
