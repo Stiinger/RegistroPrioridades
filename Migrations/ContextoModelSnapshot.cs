f93a916b37a1b97667a1c7eeac66f1a9f6246c48
Stiinger <ortegantony29@gmail.com>
2024-01-29 12:23:55 -0400

    Modificar Contexto - Actualizar base de datos.

--- Migrations/ContextoModelSnapshot.cs
+++ Migrations/ContextoModelSnapshot.cs
@@ -18,111 +18,112 @@ namespace RegistroPrioridades.Migrations
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
 
             modelBuilder.Entity("RegistroPrioridades.Models.Cliente", b =>
-                {
-                    b.Property<int>("ClienteId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<int>("ClienteId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Celular")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Celular")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("Direccion")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("TEXT");
+                b.Property<string>("Direccion")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("Email")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Email")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("Nombres")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("TEXT");
+                b.Property<string>("Nombres")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("RNC")
-                        .IsRequired()
-                        .HasMaxLength(9)
-                        .HasColumnType("TEXT");
+                b.Property<string>("RNC")
+                    .IsRequired()
+                    .HasMaxLength(9)
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("Telefono")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Telefono")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("ClienteId");
+                b.HasKey("ClienteId");
 
-                    b.ToTable("Clientes");
-                });
+                b.ToTable("Clientes");
+            });
 
             modelBuilder.Entity("RegistroPrioridades.Models.Prioridad", b =>
-                {
-                    b.Property<int>("PrioridadId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<int>("PrioridadId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Descripcion")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Descripcion")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<int?>("DiasCompromiso")
-                        .IsRequired()
-                        .HasColumnType("INTEGER");
+                b.Property<int?>("DiasCompromiso")
+                    .IsRequired()
+                    .HasColumnType("INTEGER");
 
-                    b.HasKey("PrioridadId");
+                b.HasKey("PrioridadId");
 
-                    b.ToTable("Prioridades");
-                });
+                b.ToTable("Prioridades");
+            });
 
             modelBuilder.Entity("RegistroPrioridades.Models.Sistema", b =>
-                {
-                    b.Property<int>("SistemaId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<int>("SistemaId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Nombre")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Nombre")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("SistemaId");
+                b.HasKey("SistemaId");
 
-                    b.ToTable("Sistemas");
-                });
+                b.ToTable("Sistemas");
+            });
 
             modelBuilder.Entity("RegistroPrioridades.Models.Ticket", b =>
-                {
-                    b.Property<int>("TicketId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<int>("TicketId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Asunto")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Asunto")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<int>("ClienteId")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("ClienteId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Descripcion")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Descripcion")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("Fecha")
-                        .HasColumnType("TEXT");
+                b.Property<string>("Fecha")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<int>("PrioridadId")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("PrioridadId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<int>("SistemaId")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("SistemaId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("SolicitadoPor")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("SolicitadoPor")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("TicketId");
+                b.HasKey("TicketId");
 
-                    b.ToTable("Tickets");
-                });
+                b.ToTable("Tickets");
+            });
 #pragma warning restore 612, 618
         }
     }
