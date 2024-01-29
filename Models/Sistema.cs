ee52abdcdb9d11990201c89a0a3f0ed77342243e
Stiinger <ortegantony29@gmail.com>
2024-01-29 12:09:51 -0400

    Crear modelos.

---
+++ Models/Sistema.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPrioridades.Models
+{
+	public class Sistema
+	{
+		[Key]
+		public int SistemaId { get; set; }
+
+		[Required(ErrorMessage = "Campo requerido.")]
+		public string Nombre { get; set; } = string.Empty;
+	}
+}
