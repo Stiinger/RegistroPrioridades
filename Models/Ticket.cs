0c4dc06c11a1d4496cc0cc0a0204b998a7613c04
Stiinger <ortegantony29@gmail.com>
2024-01-29 03:16:50 -0400

    Crear modelos: Sistema y Ticket.

---
+++ Models/Ticket.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroPrioridades.Models
+{
+	public class Ticket
+	{
+        [Key]
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una fecha.")]
+		[DataType(DataType.Date)]
+		public string Fecha { get; set; } = string.Empty;
+		[ForeignKey("Clientes")]
+		[Required(ErrorMessage = "Campo requerido.")]
+		public int ClienteId { get; set; }
+		[ForeignKey("Sistemas")]
+		[Required(ErrorMessage = "Campo requerido.")]
+		public int SistemaId { get; set; }
+		[ForeignKey("Prioridades")]
+		[Required(ErrorMessage = "Campo requerido.")]
+		public int PrioridadId { get; set; }
+		[Required(ErrorMessage = "Campo requerido.")]
+		public string SolicitadoPor { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Campo requerido.")]
+		public string Asunto { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Campo requerido.")]
+
+		public string Descripcion { get; set; } = string.Empty;
+    }
+}
